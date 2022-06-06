ffe0945cb07f4aed3ac817a228209bd392704bcb
dbonafin <demianbonafina@gmail.com>
2022-06-06 16:58:04 +0200

    data collect done

--- js/script.js
+++ js/script.js
@@ -1,20 +1,57 @@
-const userName = document.getElementById("user-name").value;
-console.log ("userName");
-
-const userKm = document.getElementById ("user-km").value;
-console.log ("userKm");
-
-const standardPrice = parseInt (userKm * 0.21);
-console.log ("standardPrice");
-
 
 const createButton = document.getElementById ("create-button");
 
 createButton.addEventListener("click",
     function() {
-        alert("ciao");
+
+        // DATA COLLECT
+        const userName = document.getElementById("user-name").value;
+        console.log(userName);
+
+        const userKm = parseInt (document.getElementById ("user-km").value);
+        console.log(userKm);
+
+        const userAge = document.getElementById ("user-age").value;
+        console.log(userAge);
+
+        // DIFFERENT TICKET PRICES
+        
+        let standardPrice = parseInt (userKm * 0.21);
+
+        if (userAge === "minorenne") {
+            standardPrice = standardPrice - (standardPrice * 0.20);
+        } else if (userAge === "over") {
+            standardPrice = standardPrice - (standardPrice * 0.40);
+        }
+
+        console.log(standardPrice);
+
+        // TICKET RESULT 
+
+        document.getElementById("ticket-name").innerHTML = `Nome passaggero ${userName}`;
+        document.getElementById("ticket-price").innerHTML = `Costo biglietto ${standardPrice}`;
+
+
     }
 );
 
 
- 
+// const cancelButton = document.getElementById ("cancel-button");
+
+// cancelButton.addEventListener("click",
+//     function() {
+
+//         // DATA RESET
+//         const userName = document.getElementById("user-name").value = "";
+
+//         const userKm = parseInt (document.getElementById ("user-km").value = "");
+
+//         const userAge = document.getElementById ("user-age").value = "Maggiorenne";
+
+
+//         // TICKET RESULT 
+
+//     }
+// );
+
+
